d3f59c0ddd1df940bdc0ae253db9487c6a0743b3
marwanahmed25 <marwan4125881@gmail.com>
2022-07-21 00:22:16 +0200

    payment2

--- src/handlars/volanteer_history.ts
+++ src/handlars/volanteer_history.ts
@@ -1,12 +1,14 @@
 import { Application, Response, Request } from 'express';
 import jwt from 'jsonwebtoken';
 import { volantntary_history, VolantntaryHistory } from '../models/volantntary_history';
+import { Charity } from '../models/charity';
 import parseJwt from '../utils/jwtParsing';
 import config_ from '../config/config';
 
 
 const secret: string = config_.token as unknown as string;
 const volanteer_obj = new VolantntaryHistory();
+const charity_obj = new Charity();
 //return all brands in database
 async function index(req: Request, res: Response) {
     
@@ -36,13 +38,13 @@ async function create(req: Request, res: Response) {
     try {
         const permession = jwt.verify(token,secret);
         const us = parseJwt(token);
-        //console.log(us.user);
-          
+        console.log(us.user);
+        const amount = req.body.amount;
         //if admin or super admin the changes will occure to the brand
         if (permession) {
             const c: volantntary_history = {
                 volanteer_id: us.user.id,
-                amount: req.body.amount,
+                amount: amount,
                 charity_case_id: req.body.charity_case_id,
             };
 
@@ -50,6 +52,12 @@ async function create(req: Request, res: Response) {
             
             //create new brand to the database and return new data
             const result = await volanteer_obj.create(c);
+
+
+            const charity_exist = await charity_obj.show(req.body.charity_case_id);
+            charity_exist.remaining = charity_exist.remaining - amount;
+            await charity_obj.update(charity_exist);
+
             res.status(200).json(result);
         } else res.status(400).json('Not allowed this for you!!');
 
